86b4f3f7b4f7118c7164210e4a0365ec23a65d9b
Aiko Ichikura <ichikura@jsk.imi.i.u-tokyo.ac.jp>
2024-07-10 11:55:16 +0900

    frameを得る方法を発見？

--- pepper/painting_motion/highlevel_soak.py
+++ pepper/painting_motion/highlevel_soak.py
@@ -15,6 +15,15 @@ import asyncio
 import logging
 from core import Pepper
 
+#これでframeの変化を求められそう
+def print_human(humans):
+    try:
+        human = humans[0]
+        frame = human.headFrame.value()
+        print(frame)
+    except IndexError as e:
+        print("no human")
+    
 if __name__ == "__main__":
     print("started")
     pepper = Pepper()
@@ -23,6 +32,10 @@ if __name__ == "__main__":
         pepper.AL_set("disabled")
         time.sleep(3.0)
     #pepper.AL_set("solitary")
+    humansAround = pepper.human_awareness.humansAround
+    humans = humansAround.value()
+    humansAround.connect(lambda humans: print_human(humans))
+    
     pepper.init_pose()
     ##start tracking
 
@@ -32,13 +45,13 @@ if __name__ == "__main__":
     count = 1
     while not command == "end":
         ear_led = random.randint(0,1)
-        if ear_led == 0 or count == 3: #描くモードon
+        if ear_led == 0 or count == 1: #描くモードon
             print("helping mode ON")
             pepper.change_led("EarLeds",0,0,255,0.5)
             json_name = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')+"/json/motion.json"
             pepper.curious_sound()
-            pepper.soak_motion(json_name)
-            pepper.draw_motion(json_name)
+            #pepper.soak_motion(json_name)
+            #pepper.draw_motion(json_name)
             pepper.happy_sound()
             pepper.change_led("EarLeds",0,255,0,0.5)
             time.sleep(10)
